c38da7a8388d72f5462525aca887f265d5da3368
intxz <127887955+intxz@users.noreply.github.com>
2024-02-21 21:29:15 +0100

    changes

--- Seminario1_JS/main.js
+++ Seminario1_JS/main.js
@@ -1,14 +1,14 @@
 fetch('https://jsonplaceholder.typicode.com/comments')
-  .then(response => response.json())
-  .then(json => {
+    .then(response => response.json())
+    .then(json => {
     const comments = json;
 
     const resFilter_postId = comments.filter(comment => comment.postId == 1);
     console.log("All users with postId = 1");
-    console.log("All users with" + resFilter_postId);
+    console.log(resFilter_postId);
 
     const resFilter_end = comments.filter(comment => comment.email.endsWith('.com'));
-    console.log("All users hallo with a email ended in .com");
+    console.log("All users with a email ended in .com");
     console.log(resFilter_end);
     
     const resMap_email = comments.map(comment => {
